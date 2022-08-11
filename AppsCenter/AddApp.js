21292887a1e7c3f68e658451321602987785ef6d
05GuyDray <nadav8.tch05@gmail.com>
2022-08-11 15:02:25 +0300

    need to start serch bar

--- AppsCenter/AddApp.js
+++ AppsCenter/AddApp.js
@@ -1,5 +1,12 @@
 const MIN_APP_NAME_LENGTH = 4;
 const MAX_APP_NAME_LENGTH = 30;
+const MAX_DESC_LENGTH = 500;
+const MAX_COM_NAME_LENGTH = 30;
+const MAX_IMG_URL_LENGTH = 300;
+
+const DEFOLT_COMPNY_NAME_MSG = "this app does not have a company";
+const DEFOLT_DESC_MSG = "this app does not have a description";
+const DEFOLT_URL = `Help.png`
 
 const addItemToTheList = (data) => {
     localStorage.setItem('applications', JSON.stringify(JSON.parse(localStorage.getItem('applications')).concat(data)));
@@ -16,35 +23,85 @@ document.addEventListener("DOMContentLoaded", () => {
 		
 });
 
-let inputValidationById = (id, Condtion) =>{
+const inputValidationById = (id, Condtion) =>{
     let element =  document.getElementById(id);
     if (Condtion){
-        if(!element.classList.contains("is-invalid")){element.classList.add("is-invalid");}
-        if(element.classList.contains("is-valid")){element.classList.remove("is-valid")} 
-    } else {
         if(!element.classList.contains("is-valid")){element.classList.add("is-valid");}
         if(element.classList.contains("is-invalid")){element.classList.remove("is-invalid")} 
+    } else {
+        if(!element.classList.contains("is-invalid")){element.classList.add("is-invalid");}
+        if(element.classList.contains("is-valid")){element.classList.remove("is-valid")}
     }
 }  
 
+let isThingInLengthRange = (thing , min, max ) =>{
+    return `${thing}`.length >= min &&  `${thing}`.length < max ;
+}
 
-let inputValidationAppName = (value) => {
+const inputValidationAppName = (value) => {
     let inputLength = (value + '').length;
-    let condtion = inputLength < MIN_APP_NAME_LENGTH || inputLength > MAX_APP_NAME_LENGTH;
+    let condtion = inputLength  >= MIN_APP_NAME_LENGTH && inputLength <= MAX_APP_NAME_LENGTH;
     inputValidationById("name",condtion);
 
 }
 
-let inputValidationAppPrice = (value) =>{
-    let condtion = isNaN(value) && p;
+const inputValidationAppPrice = (value) =>{
+    let condtion = !isNaN(value);
     inputValidationById("price",condtion);
 
 }
 
+const isDataFormValid = (data) =>{
+    let isAppNameValid = data.name.length >= MIN_APP_NAME_LENGTH && data.name.length  <= MAX_APP_NAME_LENGTH;
+   
+    let isPriceNum = !isNaN(data.price);
+   
+    let isAppDescritionInValidRange = isThingInLengthRange(data.description,0,MAX_DESC_LENGTH);
 
-let onSubmit = (event) => {
+    let isCompnyNameInValidRange = isThingInLengthRange(data.companyName,0,MAX_COM_NAME_LENGTH);
+   
+    let IsImgUrlInValidRange  =  isThingInLengthRange(data.imageUrl,0,MAX_IMG_URL_LENGTH);
+  
+    return isPriceNum && isAppNameValid && isAppDescritionInValidRange && isCompnyNameInValidRange && IsImgUrlInValidRange;    
+} 
+
+const makeDataApprpriatToInputIntoSys = (data) =>{
+    let imgUrl;
+    let companyName;
+    let desc;
+    if(data.imageUrl  == '' ){
+        imgUrl = DEFOLT_URL;
+    }else{imgUrl = data.imageUrl};
+    
+    if(data.companyName == ''){
+        companyName = DEFOLT_COMPNY_NAME_MSG;
+    }else{companyName = data.companyName};
+
+    if(data.description == ''){
+        desc = DEFOLT_DESC_MSG;
+    }else{desc = data.description};
+
+     return {
+        'id': `${getNextId()}`,
+        'imageUrl': `${imgUrl}`,
+        'name': `${data.name}`,
+        'price': `${data.price}`,
+        'desc': `${desc}`,
+        'companyName': `${companyName}`
+    }
+}
+
+const onSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const formProps = Object.fromEntries(formData);
-    console.log(formProps);
+
+    if(!isDataFormValid(formProps)) {
+    document.getElementById("error msg").innerHTML = "Oops,something went wrong! <br> Check your fields again.";
+    }else{
+        addItemToTheList(makeDataApprpriatToInputIntoSys(formProps));
+        window.location.href='MainPage.html';
+        
+    }
+
 }
--- AppsCenter/main.js
+++ AppsCenter/main.js
@@ -1,6 +1,4 @@
-const NO_COMPNY_NAME_MSG = "this app does not have a company";
-const NO_DESC_MSG = "this app does not have a description";
-const NO_IMAGE_URL_DEFOLT_URL = `images\\Help.png`
+
 
 const getData = () => {
     if (localStorage.getItem('applications') == null) {
@@ -24,24 +22,21 @@ const makeApplicationsHtml = (data) =>{
     return applectionsHTML;
 }
 
-// const makeAppDataValid = (appData) => {
-//     if(!ImageExist(appData.imageUrl)){
-//         appData.imageUrl = NO_IMAGE_URL_DEFOLT_URL;
-//     }
-//     if(String(appData.companyName).length == 0){
-//         appData.companyName = NO_COMPNY_NAME_MSG;
-//     }
-//     if(String(appData.desc) == 0){
-//         appData.desc = NO_DESC_MSG;
-//     }
-//     return appData;
-// }
+
 
 const makeAppHtml = (appData) => {
+    const DEFOLT_URL = `Help.png`
+    let imageurl;
+    if (appData.imageUrl == DEFOLT_URL ) {
+         imageurl = `./images/Help.png`
+        }else{
+            imageurl = `./images/${appData.id}/${appData.imageUrl}`
+     } 
+    
     return ` <div class="row app-margin"></div>
     <div class = "row">
         <div class="col col-sm-3" ></div> 
-        <div class="col col-sm-1 image-center "> <img src="\\images\\${appData.id}\\${appData.imageUrl}" class = "img-logo" width="100" height="100"></div>
+        <div class="col col-sm-1 image-center "> <img src="${imageurl}" class = "img-logo" width="100" height="100"></div>
         <div class=" col col-sm-7 ">
             <div class = "row">
             <div class = "col-sm-12 no-paddin"><h2 class= " text">${appData.name}</h2></div>
@@ -54,12 +49,4 @@ const makeAppHtml = (appData) => {
 }
 
 
-const loadLogo = () => {
-
-    let logo =  `<div class="row"><p><p></div><!-- fix it and add a good border -->
-    <img  class = "mx-auto d-block" src="images\AppsCenter.png" alt="logo"></img>`
-    document.getElementsByClassName().forEach(
-        
-    )
-}
 
